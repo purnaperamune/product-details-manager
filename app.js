445952a4408812b70ecb8ad2168a0b71293fdf30
Purna Peramune <purnaperamune@gmail.com>
2023-01-20 10:27:57 +0530

    Included swagger doc
    Designed the swagger doc for previously implemented endpoints.

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const config = require("./config");
 const app = express();
 const productsRouter = require("./src")
 
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDoc = YAML.load("./api-docs/swagger.yaml")
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/v1/products/", productsRouter)
