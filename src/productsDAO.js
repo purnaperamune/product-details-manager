4ee9547e1bf4fbf247e01db4c2803ce7c71e2bdb
Purna Peramune <purnaperamune@gmail.com>
2023-01-20 09:26:39 +0530

    Bug Fixes
    Changes done in the process of reading products by its id.

--- src/productsDAO.js
+++ src/productsDAO.js
@@ -15,18 +15,18 @@ const getProducts = function(done){
 
 const getProductById = function(id, done){
     console.log(id)
-    // functoin to get ther product by id
+    // functoin to get the product by id
     fs.readFile("src/products.json", (err, data) => {
         if(err){
             return done("ERROR")
         }
         const fileContent = JSON.parse(data)
         const prodId = parseInt(id)
-
         const productIds = fileContent.map(p => p.id)
         
         if(productIds.includes(prodId)){
-            return done(undefined, fileContent[prodId-1])
+            const index = fileContent.findIndex((obj) => obj.id === prodId)
+            return done(undefined, fileContent[index])
         }
         else{
             return done("Product Id does not exist!")
@@ -55,18 +55,15 @@ const insertProduct = function(productDetails, done){
                 }
             })
         }
-        
     })
 }
 
 const updateProduct = function(productDetails, done){
-    // function to update product
+    // function to update existing product
     fs.readFile("src/products.json", (err, data) => {
         const prodId = parseInt(productDetails.id)
         const fileContent = JSON.parse(data)
         const productIds = fileContent.map(p => p.id)
-
-        
         
         if(productIds.includes(prodId)){
             const index = fileContent.findIndex((obj) => obj.id === prodId);
@@ -90,15 +87,12 @@ const updateProduct = function(productDetails, done){
 
 const deleteProduct = function(id, done){
     // function to delete products
-    console.log(id)
-    // functoin to get ther product by id
     fs.readFile("src/products.json", (err, data) => {
         if(err){
             return done("ERROR")
         }
         const fileContent = JSON.parse(data)
         const prodId = parseInt(id)
-
         const productIds = fileContent.map(p => p.id)
         
         if(productIds.includes(prodId)){
